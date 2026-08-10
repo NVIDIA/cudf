6d6264eb0c3404b885914f69dc837b4a84d07e0e
Robert Maynard <rmaynard@nvidia.com>
2026-08-10 12:24:11 -0400

    Correct alignment deprecation warnings/errors

--- java/src/main/native/src/RmmJni.cpp
+++ java/src/main/native/src/RmmJni.cpp
@@ -1449,7 +1449,8 @@ JNIEXPORT jlong JNICALL Java_ai_rapids_cudf_Rmm_allocFromFallbackPinnedPool(JNIE
   JNI_TRY
   {
     cudf::jni::auto_set_device(env);
-    void* ret = cudf::get_pinned_memory_resource().allocate(cudf::get_default_stream(), size);
+    void* ret = cudf::get_pinned_memory_resource().allocate(
+      cudf::get_default_stream(), size, rmm::CUDA_ALLOCATION_ALIGNMENT);
     return reinterpret_cast<jlong>(ret);
   }
   JNI_CATCH(env, 0);
@@ -1465,7 +1466,8 @@ JNIEXPORT void JNICALL Java_ai_rapids_cudf_Rmm_freeFromFallbackPinnedPool(JNIEnv
   {
     cudf::jni::auto_set_device(env);
     void* cptr = reinterpret_cast<void*>(ptr);
-    cudf::get_pinned_memory_resource().deallocate(cudf::get_default_stream(), cptr, size);
+    cudf::get_pinned_memory_resource().deallocate(
+      cudf::get_default_stream(), cptr, size, rmm::CUDA_ALLOCATION_ALIGNMENT);
   }
   JNI_CATCH(env, );
 }

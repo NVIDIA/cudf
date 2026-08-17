784fdfd4f03183abd5b94895e7f0242811eda77b
Vyas Ramasubramani <vyasr@nvidia.com>
2026-08-17 15:35:43 -0700

    Reduce variance reduction instantiations

--- cpp/src/reductions/std.cu
+++ cpp/src/reductions/std.cu
@@ -27,6 +27,18 @@ std::unique_ptr<cudf::scalar> standard_deviation(column_view const& col,
   return cudf::type_dispatcher(col_type, reducer(), col, output_dtype, ddof, stream, mr);
 }
 
+std::unique_ptr<cudf::scalar> variance(column_view const& col,
+                                       cudf::data_type const output_dtype,
+                                       size_type ddof,
+                                       cuda::stream_ref stream,
+                                       rmm::device_async_resource_ref mr)
+{
+  using reducer = compound::detail::element_type_dispatcher<op::variance>;
+  auto col_type =
+    cudf::is_dictionary(col.type()) ? dictionary_column_view(col).keys().type() : col.type();
+  return cudf::type_dispatcher(col_type, reducer(), col, output_dtype, ddof, stream, mr);
+}
+
 }  // namespace detail
 }  // namespace reduction
 }  // namespace cudf
--- cpp/src/reductions/var.cu
+++ cpp/src/reductions/var.cu
@@ -3,30 +3,6 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-#include "compound.cuh"
-
-#include <cudf/dictionary/dictionary_column_view.hpp>
-#include <cudf/reduction/detail/reduction_functions.hpp>
-#include <cudf/utilities/memory_resource.hpp>
-
-#include <cuda/stream>
-
-namespace cudf {
-namespace reduction {
-namespace detail {
-
-std::unique_ptr<cudf::scalar> variance(column_view const& col,
-                                       cudf::data_type const output_dtype,
-                                       size_type ddof,
-                                       cuda::stream_ref stream,
-                                       rmm::device_async_resource_ref mr)
-{
-  using reducer = compound::detail::element_type_dispatcher<op::variance>;
-  auto col_type =
-    cudf::is_dictionary(col.type()) ? dictionary_column_view(col).keys().type() : col.type();
-  return cudf::type_dispatcher(col_type, reducer(), col, output_dtype, ddof, stream, mr);
-}
-
-}  // namespace detail
-}  // namespace reduction
-}  // namespace cudf
+// The variance implementation is intentionally co-located with standard_deviation in std.cu.
+// Both reductions use the same var_std intermediate and CUB reduction shape; keeping them in one
+// translation unit avoids emitting duplicate device kernel instantiations.

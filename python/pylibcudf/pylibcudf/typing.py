2c0ab6a7bed0951a85085c708f60cb247473ff05
Vyas Ramasubramani <vyasr@nvidia.com>
2026-08-18 14:22:56 -0700

    Improve pylibcudf source typing for generated stubs

--- python/pylibcudf/pylibcudf/typing.py
+++ python/pylibcudf/pylibcudf/typing.py
@@ -1,37 +1,54 @@
 # SPDX-FileCopyrightText: Copyright (c) 2026, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
 
-from typing import Protocol, TypeAlias, TypedDict
+from __future__ import annotations
+
+from typing import Any, Protocol, TypeAlias, TypedDict
 
 from rmm.pylibrmm.stream import Stream
 
 
 class HasCudaStream(Protocol):
-    def __cuda_stream__(self) -> object: ...
+    def __cuda_stream__(self) -> tuple[int, int]: ...
 
 
 CudaStreamLike: TypeAlias = Stream | HasCudaStream
 
 
 class ArrayInterfaceBase(TypedDict):
-    data: tuple[int, bool]
     shape: tuple[int, ...]
     typestr: str
+    data: None | tuple[int, bool]
     version: int
+    strides: None | tuple[int, ...]
+    descr: None | list[tuple[Any, ...]]
+
+
+class ArrayInterface(ArrayInterfaceBase):
+    mask: None | "SupportsArrayInterface"
+
+
+class CudaArrayInterface(ArrayInterfaceBase):
+    stream: None | int
+    mask: None | "SupportsCudaArrayInterface"
 
 
 class SupportsCudaArrayInterface(Protocol):
     @property
-    def __cuda_array_interface__(self) -> ArrayInterfaceBase: ...
+    def __cuda_array_interface__(
+        self,
+    ) -> CudaArrayInterface | dict[str, Any]: ...
 
 
 class SupportsArrayInterface(Protocol):
     @property
-    def __array_interface__(self) -> ArrayInterfaceBase: ...
+    def __array_interface__(self) -> ArrayInterface | dict[str, Any]: ...
 
 
 __all__ = [
+    "ArrayInterface",
     "ArrayInterfaceBase",
+    "CudaArrayInterface",
     "CudaStreamLike",
     "HasCudaStream",
     "SupportsArrayInterface",
